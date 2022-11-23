15da93f7537686f4da941acc04f68896a52abef9
Almina Brulić <abrulic1@etf.unsa.ba>
2022-11-23 18:49:50 +0100

    Uradjena funkcija za obracun postotka prisustva. Radjena i funkcija za iscrtavanje tabele ali nije zavrsena, potrebno dodati iscrtavanje aktuelne sedmice

--- scripts/TabelaPrisustvo.js
+++ scripts/TabelaPrisustvo.js
@@ -1,52 +1,78 @@
 export const TabelaPrisustvo = function (divRef, podaci) {
-   
-    //Ako se nalazi nesto u divu, treba isprazniti sadrzaj
      divRef.innerHtml="";
-
-    //privatni atributi modula
     let trenutnaSedmica=0; 
 
-    //implementacija metoda
     const sljedecaSedmica = function () {
     }
     const prethodnaSedmica = function () {
     }
 
-    function crtanjeTabele(podaci){
-        const table = document.createElement("table");
-        const header = document.createElement("tr");
-        const nameAndSurname = document.createElement("th");
-        const index = document.createElement("th");
-        const week = document.createElement("th");
-        table.classList.add("glavna-tabela");
-
-        // dodaj klase ovim headerima itd
-        // dodaj values sto dole nadjes kao child i to je to
-        table.append(header);
-        header.appendChild(nameAndSurname);
-        header.appendChild(index);
-        divRef.appendChild(table);
-
-        nameAndSurname.appendChild(document.createTextNode("Ime i prezime"));
-        index.appendChild(document.createTextNode("Indeks"));
-        const ukupnoEvidentiranihSedmica = podaci.prisustva[podaci.prisustva.length - 1].sedmica;
-        var i=0;
-        for(i = 0; i<ukupnoEvidentiranihSedmica; i++){
-            const sedmice = document.createElement("th");
-            header.appendChild(sedmice);
-            sedmice.appendChild(document.createTextNode(i+1));
-        }
-        trenutnaSedmica=i;
-        header.appendChild(week);
-        week.appendChild(document.createTextNode((i+1).toString().concat(" ", " - ", (15-i).toString())));
-    }
-
-    crtanjeTabele(podaci);
+    crtanjeTabele(divRef,podaci, trenutnaSedmica);
 
     return {
     sljedecaSedmica,
     prethodnaSedmica
     }
+};
+
+
+
+function crtanjeTabele(divRef,podaci,trenutnaSedmica){
+    const table = document.createElement("table");
+    const header = document.createElement("tr");
+    const nameAndSurname = document.createElement("th");
+    const index = document.createElement("th");
+    const buduceSedmice = document.createElement("th");
+    table.classList.add("glavna-tabela");
+
+    table.append(header);
+    header.appendChild(nameAndSurname);
+    header.appendChild(index);
+    divRef.appendChild(table);
+    nameAndSurname.appendChild(document.createTextNode("Ime i prezime"));
+    index.appendChild(document.createTextNode("Indeks"));
+    
+    //ukupnoEvidentiranihSedmica = podaci.prisustva[podaci.prisustva.length - 1].sedmica;
+    let i = 0;
+    for(i = 0; i<podaci.prisustva[podaci.prisustva.length - 1].sedmica; i++){
+        const sedmice = document.createElement("th");
+        header.appendChild(sedmice);
+        sedmice.appendChild(document.createTextNode(i+1));
+    }
+    trenutnaSedmica=i;
+
+    for(let i = 0; i<podaci.studenti.length; i++){
+        let row =  table.insertRow(-1);
+        row.insertCell(0).appendChild(document.createTextNode(podaci.studenti[i].ime));
+        row.insertCell(1).appendChild(document.createTextNode(podaci.studenti[i].index));
+        if(i===trenutnaSedmica){
+        iscrtajTrenutnuSedmicu(divRef, podaci, trenutnaSedmica);
+        continue;
+        }
+        if(i===podaci.prisustva[podaci.prisustva.length - 1].sedmica)
+        break;
+        for(let j=0; j<podaci.prisustva.length; j++){
+            if(podaci.prisustva[j].index===podaci.studenti[i].index){
+                row.insertCell().appendChild(document.createTextNode(vratiPostotakPrisustva(j,podaci)));
+            }
+        }
+        
+    }
+    header.appendChild(buduceSedmice);
+    buduceSedmice.appendChild(document.createTextNode((i+1).toString().concat(" ", " - ", (14).toString())));
+};
+
+
+
+function iscrtajTrenutnuSedmicu(divRef, podaci, trenutnaSedmica){
 
 };
+
+function vratiPostotakPrisustva(j,podaci){
+        let obracun = ((podaci.prisustva[j].predavanja + podaci.prisustva[j].vjezbe)*1.0)/(podaci.brojPredavanjaSedmicno + podaci.brojVjezbiSedmicno);
+        obracun=obracun*100;
+        return obracun.toString().concat("%");
+};
+
+
 
