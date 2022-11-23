9c246280725d8e0b35e0e459d65a16e0fb5f0879
Almina Brulić <abrulic1@etf.unsa.ba>
2022-11-23 17:40:58 +0100

    Uradjen import-export za module. Zapoceto crtanje i popunjavanje tabele za 1. zadatak

--- scripts/TabelaPrisustvo.js
+++ scripts/TabelaPrisustvo.js
@@ -1,21 +1,52 @@
-let TabelaPrisustvo = function (divRef, podaci) {
+export const TabelaPrisustvo = function (divRef, podaci) {
+   
     //Ako se nalazi nesto u divu, treba isprazniti sadrzaj
-    if(divRef.childNodes.length!=0)
-    divRef.innerHtml="";
+     divRef.innerHtml="";
 
     //privatni atributi modula
-    var trenutnaSedmica=null; //sad za sad je null
+    let trenutnaSedmica=0; 
 
     //implementacija metoda
-    let sljedecaSedmica = function () {
+    const sljedecaSedmica = function () {
     }
-    let prethodnaSedmica = function () {
+    const prethodnaSedmica = function () {
     }
 
+    function crtanjeTabele(podaci){
+        const table = document.createElement("table");
+        const header = document.createElement("tr");
+        const nameAndSurname = document.createElement("th");
+        const index = document.createElement("th");
+        const week = document.createElement("th");
+        table.classList.add("glavna-tabela");
+
+        // dodaj klase ovim headerima itd
+        // dodaj values sto dole nadjes kao child i to je to
+        table.append(header);
+        header.appendChild(nameAndSurname);
+        header.appendChild(index);
+        divRef.appendChild(table);
+
+        nameAndSurname.appendChild(document.createTextNode("Ime i prezime"));
+        index.appendChild(document.createTextNode("Indeks"));
+        const ukupnoEvidentiranihSedmica = podaci.prisustva[podaci.prisustva.length - 1].sedmica;
+        var i=0;
+        for(i = 0; i<ukupnoEvidentiranihSedmica; i++){
+            const sedmice = document.createElement("th");
+            header.appendChild(sedmice);
+            sedmice.appendChild(document.createTextNode(i+1));
+        }
+        trenutnaSedmica=i;
+        header.appendChild(week);
+        week.appendChild(document.createTextNode((i+1).toString().concat(" ", " - ", (15-i).toString())));
+    }
+
+    crtanjeTabele(podaci);
 
     return {
-    sljedecaSedmica: sljedecaSedmica,
-    prethodnaSedmica: prethodnaSedmica
+    sljedecaSedmica,
+    prethodnaSedmica
     }
-    
-    };
+
+};
+
