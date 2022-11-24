b09ba16fce1d5df933b8bbba5e51d4048c5c0aab
Almina Brulić <abrulic1@etf.unsa.ba>
2022-11-24 01:21:29 +0100

    Uradjeno iscrtavanje i popunjavanje tabele za prisustvo

--- scripts/TabelaPrisustvo.js
+++ scripts/TabelaPrisustvo.js
@@ -1,5 +1,5 @@
 export const TabelaPrisustvo = function (divRef, podaci) {
-     divRef.innerHtml="";
+    divRef.textContent="";
     let trenutnaSedmica=0; 
 
     const sljedecaSedmica = function () {
@@ -18,55 +18,162 @@ export const TabelaPrisustvo = function (divRef, podaci) {
 
 
 function crtanjeTabele(divRef,podaci,trenutnaSedmica){
-    const table = document.createElement("table");
+    //ISCRTAVANJE HEADERA
+    let table = document.createElement("table");
+    table.classList.add("glavna-tabela");
     const header = document.createElement("tr");
+    table.append(header);
+
     const nameAndSurname = document.createElement("th");
     const index = document.createElement("th");
-    const buduceSedmice = document.createElement("th");
-    table.classList.add("glavna-tabela");
-
-    table.append(header);
     header.appendChild(nameAndSurname);
     header.appendChild(index);
+
     divRef.appendChild(table);
     nameAndSurname.appendChild(document.createTextNode("Ime i prezime"));
     index.appendChild(document.createTextNode("Indeks"));
-    
-    //ukupnoEvidentiranihSedmica = podaci.prisustva[podaci.prisustva.length - 1].sedmica;
+
     let i = 0;
+    trenutnaSedmica = podaci.prisustva[podaci.prisustva.length - 1].sedmica;  //zadnja zabiljezena sedmica bi trebala da bude (ne garantuje se da ce tim redoslijedom biti poredani, ali da ne bismo morali prolaziti kroz citava prisustva isl. testirat cemo nad sortiranim)
+
     for(i = 0; i<podaci.prisustva[podaci.prisustva.length - 1].sedmica; i++){
-        const sedmice = document.createElement("th");
-        header.appendChild(sedmice);
-        sedmice.appendChild(document.createTextNode(i+1));
-    }
-    trenutnaSedmica=i;
-
-    for(let i = 0; i<podaci.studenti.length; i++){
-        let row =  table.insertRow(-1);
-        row.insertCell(0).appendChild(document.createTextNode(podaci.studenti[i].ime));
-        row.insertCell(1).appendChild(document.createTextNode(podaci.studenti[i].index));
-        if(i===trenutnaSedmica){
-        iscrtajTrenutnuSedmicu(divRef, podaci, trenutnaSedmica);
+       if(i==trenutnaSedmica-1){
+        const sedmica = document.createElement("th");
+        sedmica.colSpan=podaci.brojPredavanjaSedmicno+podaci.brojVjezbiSedmicno;
+        header.appendChild(sedmica);
+        sedmica.appendChild(document.createTextNode(i+1));
         continue;
-        }
-        if(i===podaci.prisustva[podaci.prisustva.length - 1].sedmica)
-        break;
-        for(let j=0; j<podaci.prisustva.length; j++){
-            if(podaci.prisustva[j].index===podaci.studenti[i].index){
-                row.insertCell().appendChild(document.createTextNode(vratiPostotakPrisustva(j,podaci)));
-            }
-        }
-        
+       }
+         const sedmice = document.createElement("th");
+         header.appendChild(sedmice);
+         sedmice.appendChild(document.createTextNode(i+1));
     }
-    header.appendChild(buduceSedmice);
-    buduceSedmice.appendChild(document.createTextNode((i+1).toString().concat(" ", " - ", (14).toString())));
+
+    const buduceSedmice = document.createElement("th");
+    if(podaci.prisustva[podaci.prisustva.length-1].sedmica!=14){
+       //ukupan broj sedmica je 14
+        header.appendChild(buduceSedmice);
+        buduceSedmice.appendChild(document.createTextNode((i+1).toString().concat(" ", " - ", (14).toString())));
+    }
+
+
+
+    //POPUNJAVANJE PODACIMA
+    for(let i = 0; i<podaci.studenti.length; i++){
+        let row = document.createElement("tr");
+        table.append(row);
+        row.insertCell().appendChild(document.createTextNode(podaci.studenti[i].ime));
+        row.cells[0].rowSpan=2;
+        row.insertCell().appendChild(document.createTextNode(podaci.studenti[i].index));
+        row.cells[1].rowSpan=2;
+
+        for(let j=0; j<podaci.prisustva.length; j++){
+            if(podaci.prisustva[j].sedmica===trenutnaSedmica){
+                    iscrtajZaglavljeTrenutneSedmice(divRef, podaci, trenutnaSedmica, row, table);
+                    break;
+            }
+            if(podaci.prisustva[j].index===podaci.studenti[i].index){
+                        let cel = row.insertCell(); 
+                        cel.appendChild(document.createTextNode(vratiPostotakPrisustva(j,podaci)));
+                        cel.rowSpan=2;
+               }
+        }
+      
+            //da se doda jos i celija za ove buduce sedmice
+            // let celija = document.createElement("td");
+            // row.appendChild(celija);
+            // celija.rowSpan=2;
+            
+            // row.appendChild(document.createElement("tr"));
+            // for(let k=0; k<podaci.brojPredavanjaSedmicno+podaci.brojVjezbiSedmicno; k++){
+            //     const celija = document.createElement("td");
+            //     row.appendChild(celija);
+            //    }
+
+            const celija = document.createElement("td");
+            row.appendChild(celija);
+            celija.rowSpan=2;
+            let row2 = document.createElement("tr");
+            table.append(row2);
+            let prisustvovaoPredavanjima=0;
+            let prisustvovaoVjezbama=0;
+           for(let j=0; j<podaci.prisustva.length; j++){
+                if(podaci.prisustva[j].index===podaci.studenti[i].index){
+                    prisustvovaoPredavanjima=podaci.prisustva[j].predavanja;
+                    prisustvovaoVjezbama=podaci.prisustva[j].vjezbe;
+                }
+           }
+           let brojac=0;
+            for(let a=0; a<podaci.brojPredavanjaSedmicno; a++){
+                const celija = document.createElement("td");
+                row2.appendChild(celija);
+                celija.classList="prisutan";
+                brojac++;
+                if(brojac>prisustvovaoPredavanjima)
+                celija.classList="odsutan";
+               }
+               brojac=0;
+               for(let a=0; a<podaci.brojVjezbiSedmicno; a++){
+                const celija = document.createElement("td");
+                row2.appendChild(celija);
+                celija.classList="prisutan";
+                brojac++;
+                if(brojac>prisustvovaoVjezbama)
+                celija.classList="odsutan";
+               }
+          
+    }
+
+ 
+
+   
+
+        //     if(podaci.prisustva[j].sedmica===trenutnaSedmica){
+        //     for(let a=0; a<podaci.brojPredavanjaSedmicno; a++){
+        //         let tdd = document.createElement("td");
+        //         tdd.appendChild(document.createTextNode("ok"));
+        //         table.appendChild(tdd);
+        //     }
+        //     for(let a=0; a<podaci.brojVjezbiSedmicno; a++){
+        //         let tdd = document.createElement("td");
+        //         tdd.appendChild(document.createTextNode("ok"));
+        //         table.appendChild(tdd);
+        //     }
+        //    // iscrtajTrenutnuSedmicu(divRef, podaci, trenutnaSedmica);
+        //     break;
+        //     }
+        // }
+
+        // if(podaci.prisustva[podaci.prisustva.length-1].sedmica!=14){
+        //     table.appendChild(document.createElement("td"));
+        // }
+
+        // row.insertCell();
+        // row.cells[4].rowSpan=2;
+
+        // let row2 = document.createElement("tr");
+        // row2.insertCell().appendChild(document.createTextNode("tekst"));
+        // table.appendChild(row2);
+    
 };
 
 
 
-function iscrtajTrenutnuSedmicu(divRef, podaci, trenutnaSedmica){
-
+function iscrtajZaglavljeTrenutneSedmice(divRef, podaci, trenutnaSedmica, row, table){
+    for(let i=0; i<podaci.brojPredavanjaSedmicno; i++){
+     const celija = document.createElement("td");
+     celija.classList="celijePrisustva";
+     row.appendChild(celija);
+     celija.appendChild(document.createTextNode("P" +  (i+1).toString()));
+    }
+    for(let i=0; i<podaci.brojVjezbiSedmicno; i++){
+        const celija = document.createElement("td");
+        celija.classList="celijePrisustva";
+        row.appendChild(celija);
+        celija.appendChild(document.createTextNode("V" +  (i+1).toString()));      
+       }
 };
+
 
 function vratiPostotakPrisustva(j,podaci){
         let obracun = ((podaci.prisustva[j].predavanja + podaci.prisustva[j].vjezbe)*1.0)/(podaci.brojPredavanjaSedmicno + podaci.brojVjezbiSedmicno);
--- scripts/prisustvo.js
+++ scripts/prisustvo.js
@@ -2,7 +2,8 @@ import { TabelaPrisustvo } from "./TabelaPrisustvo.js";
 
 let div = document.getElementById("divSadrzaj");
 //instanciranje
-let prisustvo = TabelaPrisustvo(div, {
+
+const object = {
 	"studenti": [{
         "ime": "Neko Nekic",
         "index": 12345
@@ -35,12 +36,42 @@ let prisustvo = TabelaPrisustvo(div, {
         "predavanja": 2,
         "vjezbe": 0,
         "index": 12346
-    }
+    },
+    {
+        "sedmica": 3,
+        "predavanja": 2,
+        "vjezbe": 2,
+        "index": 12345
+    },
+    {
+        "sedmica": 3,
+        "predavanja": 1,
+        "vjezbe": 2,
+        "index": 12346
+    },
+    {
+        "sedmica": 4,
+        "predavanja":2,
+        "vjezbe": 2,
+        "index": 12345
+    },
+    {
+        "sedmica": 4,
+        "predavanja": 0,
+        "vjezbe": 1,
+        "index": 12346
+    },
 ],
 "predmet": "Razvoj mobilnih aplikacija",
 "brojPredavanjaSedmicno": 2,
 "brojVjezbiSedmicno": 2
-});
+};
+
+
+let nazivPredmeta = document.getElementById("nazivPredmeta");
+nazivPredmeta.innerHTML= "<b>"+"Naziv predmeta: " + object.predmet + "<br>" + "Broj predavanja sedmicno: " + object.brojPredavanjaSedmicno + "<br>" + "Broj vjezbi sedmicno: " + object.brojVjezbiSedmicno + "</b>";
+//ovdje sam dodala broj predavanja i vjezbi jer nemamo informacije o ciklusu, smjeru isl. kao na prosloj spirali sto smo proizvoljno dodavali
+let prisustvo = TabelaPrisustvo(div, object);
 //pozivanje metoda
 prisustvo.sljedecaSedmica();
 prisustvo.prethodnaSedmica();
