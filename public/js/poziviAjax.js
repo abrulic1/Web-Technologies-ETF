b809d312d385db3fb832b931b59ebf266c9d6c91
abrulic1 <abrulic1@etf.unsa.ba>
2023-01-14 04:40:44 +0100

    Uradjen mali refactoring (manje pristupa memoriji u uslovima), te formatiranje fajlova. Spirala je u potpunosti sustinski gotova

--- public/js/poziviAjax.js
+++ public/js/poziviAjax.js
@@ -1,101 +1,93 @@
-const PoziviAjax = (()=>{
-    // ako postoji greška poruka se prosljeđuje u error parametar callback-a, a data je tada null
-    function impl_getPredmet(naziv,fnCallback){
+const PoziviAjax = (() => {
+   function impl_getPredmet(naziv, fnCallback) {
       var ajax = new XMLHttpRequest();
-      ajax.onreadystatechange = function() {
-	     if (ajax.readyState == 4 && ajax.status == 200){
-           // document.body.innerHTML='dfdfdfdf';
+      ajax.onreadystatechange = function () {
+         if (ajax.readyState == 4 && ajax.status == 200)
             fnCallback(null, ajax.response);
-         }
-	     if (ajax.readyState == 4 && ajax.status == 404){
+
+         if (ajax.readyState == 4 && ajax.status == 404)
             fnCallback(ajax.status, null);
-         }
-       } 
-      ajax.open('GET', '/predmet/'+naziv, false);
-       ajax.send(JSON.stringify(naziv));
-    }
-    // vraća listu predmeta za loginovanog nastavnika ili grešku da nastavnik nije loginovan
-    function impl_getPredmeti(fnCallback){
-    
-      // console.log('Pozvana je getPredmeti iz ajaxa');
+      }
+
+      ajax.open('GET', '/predmet/' + naziv, false);
+      ajax.send(JSON.stringify(naziv));
+   }
+
+
+   // vraća listu predmeta za loginovanog nastavnika ili grešku da nastavnik nije loginovan
+   function impl_getPredmeti(fnCallback) {
       var ajax = new XMLHttpRequest();
-      ajax.onreadystatechange = function() {
-	     if (ajax.readyState == 4 && ajax.status == 200){
+      ajax.onreadystatechange = function () {
+         if (ajax.readyState == 4 && ajax.status == 200)
             fnCallback(null, ajax.responseText);
-         }
-	     if (ajax.readyState == 4 && ajax.status == 404){
+
+         if (ajax.readyState == 4 && ajax.status == 404)
             fnCallback(ajax.status, null);
-         }
-       } 
+      }
+
       ajax.open("GET", "/predmeti", false);
-       ajax.send();
-    }
+      ajax.send();
+   }
 
-    
-    function impl_postLogin(username,password,fnCallback){
-      //pravim xhr objekat
-        var ajax = new XMLHttpRequest();
-
-      //otvaramo konekciju na server
+   
+   function impl_postLogin(username, password, fnCallback) {
+      var ajax = new XMLHttpRequest();
       ajax.open("POST", "/login", true);
       ajax.setRequestHeader("Content-Type", "application/json");
       const podaci = {
-        "username": username, 
-        "password": password
+         "username": username,
+         "password": password
       }
-      
+
       ajax.send(JSON.stringify(podaci, null, 2));
-    
-      ajax.onreadystatechange = function() {
-	     if (ajax.readyState == 4 && ajax.status == 200)
+      ajax.onreadystatechange = function () {
+         if (ajax.readyState == 4 && ajax.status == 200)
             fnCallback(null, ajax.responseText);
-         
-	     if (ajax.readyState == 4 && ajax.status == 404)
+
+         if (ajax.readyState == 4 && ajax.status == 404)
             fnCallback(ajax.status, null);
-         
-       } 
-    }
+      }
+   }
 
 
-    
-    function impl_postLogout(fnCallback){
-         var ajax = new XMLHttpRequest();
-         ajax.open("POST", "/logout", true);
-         ajax.setRequestHeader("Content-Type", "application/json");
-         //ovo mi vrv ni ne treba
-         ajax.send();
-         ajax.onreadystatechange = function() {
-            if (ajax.readyState == 4 && ajax.status == 200){
-               fnCallback(null, ajax.responseText);
-            }
-            if (ajax.readyState == 4 && ajax.status == 404){
-               fnCallback(ajax.status, null);
-            }
-          } 
-    }
-    //prisustvo ima oblik {sedmica:N,predavanja:P,vjezbe:V}
-    function impl_postPrisustvo(naziv,index,prisustvo,fnCallback){
+   function impl_postLogout(fnCallback) {
       var ajax = new XMLHttpRequest();
-      ajax.open("POST", "/prisustvo/predmet/"+naziv+"/student/"+index, true);
+      ajax.open("POST", "/logout", true);
       ajax.setRequestHeader("Content-Type", "application/json");
-      //ovo mi vrv ni ne treba
-      console.log('ovo vovamo iz ajaksa ',JSON.stringify(prisustvo, null, 2), ' indeks ', index, ' i naiv predmeta ', naziv);
+      ajax.send();
+      ajax.onreadystatechange = function () {
+         if (ajax.readyState == 4 && ajax.status == 200)
+            fnCallback(null, ajax.responseText);
+
+         if (ajax.readyState == 4 && ajax.status == 404)
+            fnCallback(ajax.status, null);
+      }
+   }
+
+
+   //prisustvo ima oblik {sedmica:N,predavanja:P,vjezbe:V}
+   function impl_postPrisustvo(naziv, index, prisustvo, fnCallback) {
+      var ajax = new XMLHttpRequest();
+      ajax.open("POST", "/prisustvo/predmet/" + naziv + "/student/" + index, true);
+      ajax.setRequestHeader("Content-Type", "application/json");
       ajax.send(JSON.stringify(prisustvo, null, 2));
-      ajax.onreadystatechange = function() {
-         if (ajax.readyState == 4 && ajax.status == 200){
-            console.log('iz ajaxa je response: ', ajax.response)
+      ajax.onreadystatechange = function () {
+         if (ajax.readyState == 4 && ajax.status == 200) {
+            // console.log('iz ajaxa je response: ', ajax.response)
             fnCallback(null, ajax.responseText);
          }
-         if (ajax.readyState == 4 && ajax.status == 404){
+         if (ajax.readyState == 4 && ajax.status == 404) {
             fnCallback(ajax.status, null);
          }
-       } 
-    }
-    return{
-    postLogin: impl_postLogin,
-    postLogout: impl_postLogout,
-    getPredmet: impl_getPredmet,
-    getPredmeti: impl_getPredmeti,
-    postPrisustvo: impl_postPrisustvo
-    };
-    })();
+      }
+   }
+
+
+   return {
+      postLogin: impl_postLogin,
+      postLogout: impl_postLogout,
+      getPredmet: impl_getPredmet,
+      getPredmeti: impl_getPredmeti,
+      postPrisustvo: impl_postPrisustvo
+   };
+})();
--- public/js/predmeti.js
+++ public/js/predmeti.js
@@ -1,14 +1,22 @@
 let redovi = [];
-let tabela=null;
 let buttonsContainer = null;
-let nazivPredmeta='';
+let nazivPredmeta = '';
 document.getElementById('logout').addEventListener('click', odjaviSe);
+
+function odjaviSe() {
+    PoziviAjax.postLogout((err, data) => {
+        if (err != null)
+            alert('Neispravni podaci!');
+        else
+            window.location.href = "/prijava";
+    });
+}
+
 
 window.onload = function () {
     PoziviAjax.getPredmeti((err, data) => {
-        if (err != null) {
+        if (err != null)
             alert('Neispravni podaci, ne mogu se dohvatiti predmeti za profesora!');
-        }
         else {
             data = JSON.parse(data);
             lista = document.createElement('ul');
@@ -20,16 +28,16 @@ window.onload = function () {
             }
             let nav = document.getElementById('nav');
             nav.appendChild(lista);
+
             let items = document.querySelectorAll('#lista li');
             //ne uzimam logout dugme
             for (let i = 1; i < items.length; i++)
                 items[i].onclick = function () {
-                    window.trenutnaSedmica=0;
+                    window.trenutnaSedmica = 0;
                     var current = document.getElementsByClassName('active');
                     if (current.length != 0) current[0].className = current[0].className.replace(' active', '');
                     items[i].className += " active";
-                    
-                    
+
                     PoziviAjax.getPredmet(items[i].innerHTML, (err, data) => {
                         if (err != null)
                             console.log('Ne moze se dohvatiti predmet iz poziviAjax!')
@@ -37,23 +45,14 @@ window.onload = function () {
                             data = JSON.parse(data);
                             let div = document.getElementById('tabelaPrisustva');
                             const { prethodnaSedmica, sljedecaSedmica } = TabelaPrisustvo(div, data);
-                            tabela = document.getElementById('glavna-tabela');
                             buttonsContainer = document.getElementById("dugmici");
                             dugmad(buttonsContainer, prethodnaSedmica, sljedecaSedmica);
                             redovi = document.getElementsByTagName('tr');
-                            nazivPredmeta=items[i].innerHTML;
-                            buttonsContainer.onclick = function(){
+                            nazivPredmeta = items[i].innerHTML;
+                            buttonsContainer.onclick = function () {
                                 promijeniPrisustvo(items[i].innerHTML)
                             }
-                             // tabela.addEventListener('click', function(event){
-                            // console.log('poziva se table listener'); 
-                            promijeniPrisustvo(items[i].innerHTML)
-                            // })
-                            // tabela.onclick=function(){
-                            //    console.log('poziva se table listener'); 
-                            // promijeniPrisustvo(items[i].innerHTML)
-                            // }
-                        
+                            promijeniPrisustvo(items[i].innerHTML);
                         }
                     })
                 }
@@ -62,119 +61,83 @@ window.onload = function () {
 }
 
 
-function odjaviSe() {
-    PoziviAjax.postLogout((err, data) => {
-        if (err != null) {
-            alert('Neispravni podaci!');
-        }
-        else {
-            window.location.href = "/prijava";
-        }
-    });
-}
-
-
- function promijeniPrisustvo(naziv) {
+function promijeniPrisustvo(naziv) {
+    //nisu mi od interesa redovi koji imaju podatke o imenu, prezimenu i indexu, kao i ovi sto imaju ovo P1, P2, V1, V2, V3 isl. zato +=2 u petlji
     for (let i = 0; i < redovi.length; i += 2) {
         let row = redovi[i];
-        for (let j = 0; j < row.cells.length; j++) { 
-        row.cells[j].onclick = function (event) {
-            console.log("pozvan je listener");
+        for (let j = 0; j < row.cells.length; j++) {
+            row.cells[j].onclick = function (event) {
                 let clickedCell = event.target;
                 let clickedRow;
                 let clickedColumn;
-                    let cell = row.cells[j];
-                    if (cell === clickedCell) {
-                        var thElements = document.querySelectorAll("th");
-                        // let trenutniTH = 0;
-                        for (let k = 0; k < thElements.length; k++)
-                            if (thElements[k].colSpan != 1) {
-                                // trenutniTH = k;
-                                window.trenutnaSedmica=k-1;
-                                break;
-                            }
-                        clickedRow = i;
-                        clickedColumn = j;
-                        let predavanjaUkupno = 0;
-                        let vjezbeUkupno = 0;
-                        let predavanjaPrisustvo = 0;
-                        let vjezbePrisustvo = 0;
-
-                        for (let k = 0; k < row.cells.length; k++) {
-                            if (row.cells[k].classList.contains('predavanja')) {
-                                predavanjaUkupno++;
-                                if (row.cells[k].classList.contains('prisutan')) predavanjaPrisustvo++;
-                            }
-                            else if (row.cells[k].classList.contains('vjezbe')) {
-                                vjezbeUkupno++;
-                                if (row.cells[k].classList.contains('prisutan')) vjezbePrisustvo++;
-                            }
-                        }
-                      
-                        let vjezbeSuAzurirane = 0;
-                        let predavanjaSuAzurirana = 0;
-                        if (clickedColumn >= predavanjaUkupno) vjezbeSuAzurirane = 1;
-                        else predavanjaSuAzurirana = 1;
-                       
-                        for (let k = 0; k < row.cells.length; k++) {
-                            if (clickedColumn == k) {
-                                if (row.cells[k].classList.contains('predavanja') && row.cells[k].classList.contains('prisutan')) {
-                                    predavanjaPrisustvo--;
-                                    // row.cells[k].classList.remove('prisutan');
-                                    // row.cells[k].classList.add('odsutan');
-                                    break;
-                                }
-                                else if (row.cells[k].classList.contains('predavanja') && (row.cells[k].classList.contains('odsutan') || row.cells[k].classList.contains('praznaCelija'))) {
-                                    predavanjaPrisustvo++;
-                                    // row.cells[k].classList.remove('odsutan');
-                                    // row.cells[k].classList.remove('praznaCelija');
-                                    // row.cells[k].classList.add('prisutan');
-                                    break;
-                                }
-                                else if (row.cells[k].classList.contains('vjezbe') && row.cells[k].classList.contains('prisutan')) {
-                                    vjezbePrisustvo--;
-                                    // row.cells[k].classList.remove('prisutan');
-                                    // row.cells[k].classList.add('odsutan');
-                                    break;
-                                }
-                                else if (row.cells[k].classList.contains('vjezbe') && (row.cells[k].classList.contains('odsutan') || row.cells[k].classList.contains('praznaCelija'))) {
-                                    vjezbePrisustvo++;
-                                    // row.cells[k].classList.remove('odsutan');
-                                    // row.cells[k].classList.remove('praznaCelija');
-                                    // row.cells[k].classList.add('prisutan');
-                                    break;
-                                }
-                            }
+                let cell = row.cells[j];
+                if (cell === clickedCell) {
+                    var thElements = document.querySelectorAll("th");
+                    for (let k = 0; k < thElements.length; k++)
+                        if (thElements[k].colSpan != 1) {
+                            //sedmice tj th idu indecacijom 0, 1, 2... da idu od 1, 2 itd. umanjili bismo za 2 jer nam ne trebaju "ime i prezime" i "index" kolone
+                            window.trenutnaSedmica = k - 1;
+                            break;
                         }
 
-                        //sedmice tj th idu indexacijom 0, 1, 2... da idu od 1, 2, .. umanjili bi za 2 jer nam ne trebaju ime prezime i index kolone
-                        let celija = { "sedmica": window.trenutnaSedmica, "predavanja": predavanjaPrisustvo, "vjezbe": vjezbePrisustvo };
-                        let index = redovi[clickedRow - 1].cells[1].textContent;
-                       console.log('saljemo sedmicu ', window.trenutnaSedmica, ' predavanja ', predavanjaPrisustvo, ' i vjezbe ', vjezbePrisustvo, ' i index ', index, ' i naziv ', naziv);
-                         PoziviAjax.postPrisustvo(naziv, index, celija, (err, data) => {
-                            if (err != null)
-                                console.log('ne valja');
-                            else {
-                                console.log('valja i iscrtava se ponovo sve ispocetka');
-                                    data = JSON.parse(data); 
-                                    console.log('IZ PREDMETI SE SALJU : ', data);
-                                    let div = document.getElementById('tabelaPrisustva');
-                                    const { prethodnaSedmica, sljedecaSedmica } = TabelaPrisustvo(div, data, window.trenutnaSedmica);
-                                    tabela = document.getElementById('glavna-tabela');
-                                     buttonsContainer = document.getElementById("dugmici");
-                                    dugmad(buttonsContainer, prethodnaSedmica, sljedecaSedmica);
-                                    redovi = document.getElementsByTagName('tr');
-                                     promijeniPrisustvo(naziv);
-                                     buttonsContainer.onclick = function(){
-                                    dugmad(buttonsContainer, prethodnaSedmica, sljedecaSedmica);
-                                    redovi = document.getElementsByTagName('tr');
-                                    promijeniPrisustvo(nazivPredmeta)
-}
+                    clickedRow = i;
+                    clickedColumn = j;
+                    let predavanjaUkupno = 0;
+                    let vjezbeUkupno = 0;
+                    let predavanjaPrisustvo = 0;
+                    let vjezbePrisustvo = 0;
+
+                    for (let k = 0; k < row.cells.length; k++) {
+                        const classLista = row.cells[k].classList;
+                        if (classLista.contains('predavanja')) {
+                            predavanjaUkupno++;
+                            if (classLista.contains('prisutan')) predavanjaPrisustvo++;
+                        }
+                        else if (classLista.contains('vjezbe')) {
+                            vjezbeUkupno++;
+                            if (classLista.contains('prisutan')) vjezbePrisustvo++;
+                        }
+                    }
+
+                    for (let k = 0; k < row.cells.length; k++) {
+                        if (clickedColumn == k) {
+                            const classLista = row.cells[k].classList;
+                            if (classLista.contains('predavanja') && classLista.contains('prisutan'))
+                                predavanjaPrisustvo--;
+                            else if (classLista.contains('predavanja') && (classLista.contains('odsutan') || classLista.contains('praznaCelija')))
+                                predavanjaPrisustvo++;
+                            else if (classLista.contains('vjezbe') && classLista.contains('prisutan'))
+                                vjezbePrisustvo--;
+                            else if (classLista.contains('vjezbe') && (classLista.contains('odsutan') || classLista.contains('praznaCelija')))
+                                vjezbePrisustvo++;
+                        }
+                    }
+
+                    let celija = { "sedmica": window.trenutnaSedmica, "predavanja": predavanjaPrisustvo, "vjezbe": vjezbePrisustvo };
+                    let index = redovi[clickedRow - 1].cells[1].textContent;
+                    //  console.log('saljemo sedmicu ', window.trenutnaSedmica, ' predavanja ', predavanjaPrisustvo, ' i vjezbe ', vjezbePrisustvo, ' i index ', index, ' i naziv ', naziv);
+                    PoziviAjax.postPrisustvo(naziv, index, celija, (err, data) => {
+                        if (err != null)
+                            console.log('ne valja');
+                        else {
+                            data = JSON.parse(data);
+                            //  console.log('IZ PREDMETI SE SALJU : ', data);
+                            let div = document.getElementById('tabelaPrisustva');
+                            const { prethodnaSedmica, sljedecaSedmica } = TabelaPrisustvo(div, data);
+                            buttonsContainer = document.getElementById("dugmici");
+                            dugmad(buttonsContainer, prethodnaSedmica, sljedecaSedmica);
+                            redovi = document.getElementsByTagName('tr');
+                            promijeniPrisustvo(naziv);
+                            buttonsContainer.onclick = function () {
+                                dugmad(buttonsContainer, prethodnaSedmica, sljedecaSedmica);
+                                redovi = document.getElementsByTagName('tr');
+                                promijeniPrisustvo(nazivPredmeta)
                             }
-                        })
+                        }
+                    })
                 }
+            }
         }
     }
-}
 }
 
