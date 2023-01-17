72f216c04b457838f025a4699bb887f5a92bd431
abrulic1 <abrulic1@etf.unsa.ba>
2023-01-17 04:05:24 +0100

    Otkriveni pristupni podaci bazi

--- baza.js
+++ baza.js
@@ -1,16 +1,28 @@
 const Sequelize = require('sequelize');
-require('dotenv').config();
+// require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD,{
-      host: process.env.DB_HOST,
-      dialect: process.env.DB_DIALECT,
-   pool: {
-       max: 5,
-       min: 0,
-       acquire: 30000,
-       idle: 10000
-   }
- }
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD,{
+//       host: process.env.DB_HOST,
+//       dialect: process.env.DB_DIALECT,
+//    pool: {
+//        max: 5,
+//        min: 0,
+//        acquire: 30000,
+//        idle: 10000
+//    }
+//  }
+// );
+
+const sequelize = new Sequelize('wt22', 'root', 'password',{
+  host: '127.0.0.1',
+  dialect: 'mysql',
+pool: {
+   max: 5,
+   min: 0,
+   acquire: 30000,
+   idle: 10000
+}
+}
 );
 
 
